8c654c988ba48f800bc3a475431d95463e5daa5b
Qwer2281 <6707lucas@gmail.com>
2025-04-29 22:19:26 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long i;
+		
+		for(i=1;i<10;i++)
+			System.out.println(a+" * "+i+" = "+a*i);
+		
+	}
+}
