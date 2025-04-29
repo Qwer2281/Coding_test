b14c05d686c4f3211880488ba2a2a14b78613e93
Qwer2281 <6707lucas@gmail.com>
2025-04-29 22:26:59 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		long t = sc.nextLong();
+		long i;
+		
+		for(i=1;i<=t;i++) {
+			long a = sc.nextLong();
+			long b = sc.nextLong();
+			System.out.println(a+b);
+		}
+		
+	}
+}
